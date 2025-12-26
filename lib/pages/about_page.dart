bc0c2ff7d70b543cebb0e4c69d6a9b163bd83cb3
StasyaoSan <stasyaosan93@gmail.com>
2025-12-26 19:57:46 +0500

    0.0.4v1

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -10,45 +10,50 @@ class AboutPage extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Expanded(
-          child: ListView.builder(
-            itemCount: mainProvider.infoPageDatas.datas.length,
-            itemBuilder: (context, index) {
-              final infoPageDataItem = mainProvider.infoPageDatas.datas[index];
-              if (index == 0) {
-                return Column(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadiusGeometry.circular(55),
-                      child: SizedBox(
-                        height: 300,
-                        width: 300,
-                        child: Image.asset('assets/images/701899937.jpeg'),
-                      ),
-                    ),
-                    Divider(),
-                    Text(infoPageDataItem.headertext),
-                    Text(
-                      infoPageDataItem.bodytext,
-                      textAlign: TextAlign.justify,
-                    ),
-                    Divider(),
-                  ],
-                );
-              } else {
-                return Column(
-                  children: [
-                    Text(infoPageDataItem.headertext),
-                    Text(
-                      infoPageDataItem.bodytext,
-                      textAlign: TextAlign.justify,
-                    ),
-                    Divider(),
-                  ],
-                );
-              }
-            },
-          ),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: mainProvider.infoPageDatas.datas.length,
+                itemBuilder: (context, index) {
+                  final infoPageDataItem =
+                      mainProvider.infoPageDatas.datas[index];
+                  if (index == 0) {
+                    return Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadiusGeometry.circular(55),
+                          child: SizedBox(
+                            height: 300,
+                            width: 300,
+                            child: Image.asset('assets/images/701899937.jpeg'),
+                          ),
+                        ),
+                        Divider(),
+                        Text(infoPageDataItem.headertext),
+                        Text(
+                          infoPageDataItem.bodytext,
+                          textAlign: TextAlign.justify,
+                        ),
+                        Divider(),
+                      ],
+                    );
+                  } else {
+                    return Column(
+                      children: [
+                        Text(infoPageDataItem.headertext),
+                        Text(
+                          infoPageDataItem.bodytext,
+                          textAlign: TextAlign.justify,
+                        ),
+                        Divider(),
+                      ],
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
